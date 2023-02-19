4e07ad9481da2c74acc0ca3b3743ecefd188ff17
GraniteNoah <noahbissett6@gmail.com>
2023-02-18 19:35:41 -0800

    Update q1.py

--- q1.py
+++ q1.py
@@ -1,41 +1,50 @@
-##main
-#question 1
-print("Hi, this is Pchatbot, can I talk to you?\n")
-t_char=input()
 
-if (t_char=="Y" or t_char=='y'):
-  #question 2
-  print("What is your name?\n")
-  name=input()
-  print("Nice to meet you,", name + ".")
-  
-  #question 3
-  print("How are you doing today?\n")
-  feeling=input()
-  Drive=False
-  if (feeling=="Fine" or feeling=="I'm good" or feeling=="I'm great" or feeling=="Good"):
-    print("I'm glad you're feeling well,", name + ".")
-    Drive=True
-  elif(feeling=="Bad" or feeling=="Not Okay" or feeling=="I' m not feeling good"):
-    print("Have some time to yourself to recharge!")
-    Drive=False
-  else:
-    print("I see!")
 
+
+while(True):
+    source = input()
+    if(source == "quit"):
+        break
+
+    k = int(input())
+
+    if (len(source) <= k):
+        print(source)
     
-  #question 4
-  print("How old are you?\n")
-  age=input()
+    else:
+        all = {}
+        i=0
+        j=0
+        while(i<=len(source)-j):
+            letters=""
+            j=0
+            while(j<k):
+                letters = letters + source[i+j]
+                all[i] = letters
+                j+=1
+            i += 1
+        ans1 = {}
+        i = 0
+        while(i<len(all)):
+            track = {}
+            for j in all[i]:
+                if (j not in track):
+                    track[j] = 1
+                else:
+                    track[j] += 1
+            ans1[i] = len(track)
+            i+=1
+        counter = k
+        ##pinting error
+        i=0
+        Flag=True
+        while (i<len(ans1) and Flag):
+            for j in ans1:
+                if ans1[j] == counter:
+                    print(all[j])
+                    Flag = False
+                    break
+            counter-=1    
+                
 
-  if(int(age)>18 and Drive==True):
-    print("You are ready to drive.\n")
 
-  elif(int(age)<=18 or Drive==False):
-    print("Still taking the bus!\n")
-  
-
-  
-elif(t_char=='N' or t_char=='n'):
-  print("Okay! Talk to you soon!")  
-
-  
