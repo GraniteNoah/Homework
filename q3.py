c07a4bd8f2ce28faa37bb33c69344014848e76ec
GraniteNoah <noahbissett6@gmail.com>
2023-02-18 23:34:36 -0800

    Update q3.py

--- q3.py
+++ q3.py
@@ -1,35 +1,17 @@
-def take_int():
-    print("Enter a positive integer:\n")
-    x=int(input())
-    if(x<0):
-        print("PrimeFinder ignores negative numbers!")
-        return -1
-    elif(x>=0):
-        return x
+source=input()
+f=len(source)-1
+i=0
 
-##main
-print("How many numbers do you want to check?\n")
-n = int(input())
-i = 0
-count = 0
-while(i<n):
-    prime=True
-    num = take_int()
-    
-    if(num == -1):
-        i-=1
+while(i<f):
+    Flag=False
+    if(source[i]==source[f] or ord(source[i])==ord(source[f])+32 
+    or ord(source[i])==ord(source[f])-32):
+        Flag=True
+    i+=1
+    f-=1
 
-    else:
-        if(num % 2 == 0):
-            prime = False
+if (Flag):
+    print ("Yes")
 
-        elif(num % 3 == 0):
-            prime = False
-
-        if(prime == True):
-            print(num, "is a prime number.\n")
-            count += 1
-
-    i += 1
-
-print("Total prime numbers:", count)
+else:
+    print ("No")
